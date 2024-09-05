fe3a59adb7ec014fa5dc55dc131fc053c817082f
rajesh2k18.hyd@gmail.com <rajesh2k18.hyd@gmail.com>
2024-09-05 15:59:17 +0530

    component setup and Top header, footer and menu list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, ActivatedRoute, RouterOutlet, RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { AppmaterialModule } from './appmaterial/appmaterial.module';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppmaterialModule, FormsModule, RouterLink, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Online shopping Project';
+  searchValue: any;
+  wishlist: any = [];
+  cart: any;
+
+  isUserAuthenticated: any;
+
+  constructor(
+    // private route: ActivatedRoute, private router: Router
+  ) {
+  }
+
+  ngOnInit() {
+
+    // this.usersub=this.LoginService.user.subscribe(user => {
+    // this.isUserAuthenticated = !user ? false : true; 
+    //  this.user=user;});
+
+    // this.data.currentMessage.subscribe(username => this.username = username)
+  }
+
+  onSearch() { }
+
+  logOut() { }
 }
